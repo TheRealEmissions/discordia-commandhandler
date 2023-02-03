daa8e65f440a44ee76f6b655f45c38d1f1b1f425
Liam <46268350+TheRealEmissions@users.noreply.github.com>
2023-02-03 05:30:55 +0000

    Delete out directory

--- out/src/events/CommonEvents.js
+++
@@ -1,8 +0,0 @@
-export var CommandHandlerEvents;
-(function (CommandHandlerEvents) {
-    CommandHandlerEvents["APPLICATION_COMMAND_USE"] = "applicationCommandUse";
-    CommandHandlerEvents["BUTTON_CLICK"] = "buttonClick";
-    CommandHandlerEvents["CONTEXT_MENU_USE"] = "contextMenuUse";
-    CommandHandlerEvents["SELECT_MENU_CLICK"] = "selectMenuClick";
-    CommandHandlerEvents["MODAL_SUBMIT"] = "modalSubmit";
-})(CommandHandlerEvents || (CommandHandlerEvents = {}));
